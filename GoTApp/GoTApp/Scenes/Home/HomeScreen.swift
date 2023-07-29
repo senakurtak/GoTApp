1f443fc2b8c45cbc55b76c22a3bac88579978e29
Sena Kurtak <zskurtak@gmail.com>
2023-07-29 15:09:27 +0300

    feature/added UICollectionView on homescreen

--- GoTApp/GoTApp/Scenes/Home/HomeScreen.swift
+++ GoTApp/GoTApp/Scenes/Home/HomeScreen.swift
@@ -8,20 +8,44 @@
 import SwiftUI
 
 struct HomeScreen: View {
-    
     @ObservedObject var viewModel = HomeScreenViewModel()
+    
+    var columns: [GridItem] = [
+        GridItem(.flexible(minimum: 100, maximum: 250), spacing: 0),
+        GridItem(.flexible(minimum: 100, maximum: 250), spacing: 0),
+    ]
+    
+    var cellWidth: CGFloat = 150
+    var cellHeight: CGFloat = 100
+    var padding: CGFloat = 10
     
     var body: some View {
         NavigationView {
-            List(viewModel.houses) { house in
-                NavigationLink(destination: DetailScreen(house: house)) {
-                    Text(house.name)
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 0) {
+                    ForEach(viewModel.houses) { house in
+                        NavigationLink(destination: DetailScreen(house: house)) {
+                            VStack(alignment: .leading) {
+                                Text(house.name)
+                                    .font(.headline)
+                                    .foregroundColor(Color("GoTWhite"))
+                            }
+                            .padding(padding)
+                            .frame(width: cellWidth, height: cellHeight)
+                            .background(Color("GoTLightGray"))
+                            .cornerRadius(8)
+                        }
+                        .padding(padding)
+                    }
                 }
+                .padding(padding)
             }
-
-            .listStyle(GroupedListStyle())
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color("GoTWhite"), Color("GoTDarkGray")]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+            )
+            .navigationTitle("List of Houses")
         }
-        .navigationViewStyle(StackNavigationViewStyle())
         .onAppear {
             viewModel.fetchHouseList()
         }

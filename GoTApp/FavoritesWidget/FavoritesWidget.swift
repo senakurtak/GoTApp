058a0207b0132975da1a74ebd7a9891767e348bb
Sena Kurtak <zskurtak@gmail.com>
2024-04-23 18:36:49 +0300

    feature/added widget

---
+++ GoTApp/FavoritesWidget/FavoritesWidget.swift
@@ -0,0 +1,90 @@
+//
+//  FavoritesWidget.swift
+//  FavoritesWidget
+//
+//  Created by Sena Kurtak on 23.04.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: AppIntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+    }
+
+    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: configuration)
+    }
+    
+    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        return Timeline(entries: entries, policy: .atEnd)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationAppIntent
+}
+
+struct FavoritesWidgetEntryView : View {
+    var entry: Provider.Entry
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("10")
+                    .font(.title)
+                HStack {
+                    Text("🛖")
+                        .font(.title)
+                    Text("Favorite Houses")
+                        .font(.subheadline)
+                }
+                .padding(5)
+            }
+        }
+    }
+}
+
+struct FavoritesWidget: Widget {
+    let kind: String = "FavoritesWidget"
+    
+    var body: some WidgetConfiguration {
+        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+            FavoritesWidgetEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
+        }
+    }
+}
+
+extension ConfigurationAppIntent {
+    fileprivate static var smiley: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "😀"
+        return intent
+    }
+    
+    fileprivate static var starEyes: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "🤩"
+        return intent
+    }
+}
+
+#Preview(as: .systemSmall) {
+    FavoritesWidget()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
---
+++ GoTApp/FavoritesWidget/FavoritesWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesWidgetBundle.swift
+//  FavoritesWidget
+//
+//  Created by Sena Kurtak on 23.04.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FavoritesWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FavoritesWidget()
+        FavoritesWidgetLiveActivity()
+    }
+}
